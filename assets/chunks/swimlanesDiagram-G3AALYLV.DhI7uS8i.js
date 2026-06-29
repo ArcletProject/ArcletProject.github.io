3bcab2b7c4403148ced7cebd00e18ab0119da982
RF-Tar-Railt <RF-Tar-Railt@users.noreply.github.com>
2026-06-29 09:54:21 +0000

    deploy: ArcletProject/Documents@2d0b3b0a5448912cc0af529e9cbd199da9683718

---
+++ assets/chunks/swimlanesDiagram-G3AALYLV.DhI7uS8i.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-23GEKE2U.yuVsbi62.js";import{_ as a}from"../app.CPmoRI7h.js";import"./chunk-5VM5RSS4.ieheNpbW.js";import"./chunk-XXDRQBXY.DiRGFF6V.js";import"./chunk-VR4S4FIN.DH1_RO8C.js";import"./chunk-32BRIVSS.BCoTpAyw.js";import"./channel.B7Q3vQQf.js";import"./framework.B_x9-dIc.js";import"./theme.BV0y7eg9.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
